2494f145f7d8edcc82a3510e6ad6bc13e278f9e3
Preciouskuku <preciouskukumutema@gmail.com>
2025-11-10 11:12:32 +0200

    changed subscribe

--- src/copmonents/Subscribe.jsx
+++ src/copmonents/Subscribe.jsx
@@ -4,23 +4,24 @@ const CallToAction = () => {
   return (
     <section className="flex flex-col md:flex-row items-center justify-between bg-[#F7E9DC] md:h-[300px] w-full overflow-hidden">
       {/* Left side - Image */}
-      <div className="md:w-1/2 w-full bg-[#F7E9DC] flex justify-center items-center md:h-[300px] overflow-hidden">
+      <div className="w-full md:w-1/2 flex justify-center items-center overflow-hidden">
         <img
           src="/gli.png"
           alt="Model"
-          className="object-cover h-[500px] w-full transform scale-100 hover:scale-105 transition-transform duration-700 ease-in-out"
+          className="object-cover w-full h-[350px] md:h-[300px] transform scale-100 hover:scale-105 transition-transform duration-700 ease-in-out"
         />
       </div>
 
       {/* Right side - Text */}
-      <div className="bg-[#8A5839] md:w-1/2 w-full text-center md:text-left px-6 md:px-20 py-12 md:py-0 text-white md:h-[300px] ">
-        <p className="text-2xl md:text-3xl italic font-light leading-relaxed">
+      <div className="bg-[#8A5839] w-full md:w-1/2 text-center md:text-left  md:px-5 py-0 md:py-0 text-white flex flex-col justify-center items-center md:items-start md:h-[300px]">
+        <p className="text-xl md:text-3xl italic font-light leading-relaxed">
           “Join thousands of women who trust
           <br />
           <span className="font-medium not-italic">BatanaByCleo</span> to
-          enhance their natural glow.”
+          enhance their natural 
+          <br /> glow.”
         </p>
-        <button className="mt-8 bg-white text-[#8A5839] font-semibold py-3 px-6 rounded-md shadow-sm hover:bg-[#f5f5f5] transition-all">
+        <button className="mt-20 bg-white text-[#8A5839] font-semibold py-3 px-6 rounded-md shadow-sm hover:bg-[#f5f5f5] transition-all">
           Sign Up for updates
         </button>
       </div>
